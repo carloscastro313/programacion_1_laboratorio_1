82659ed41c0af585328bba41a7fc50d587a2c7a7
carlos <castrocarlos313@gmail.com>
2019-03-22 21:02:09 -0300

    cosas

--- Nueva carpeta/Ejercico clase 2/main.c
+++ Nueva carpeta/Ejercico clase 2/main.c
@@ -5,14 +5,18 @@
 
 int main()
 {
-    int numero ,i ,pares ,positivos ,negativos ,min ,max;
+    int numero ,i ,pares ,suma ,positivos ,negativos ,min ,max ,numeroMenorPositivo ,numeroMenorNegativo ,numeroEntre ,flag;
+    float promedio ,promedioNegativos;
     char respuesta;
 
-    max=INI_MAX;
-    min=INI_MIN;
+    flag=0;
+    positivos=0;
     negativos=0;
+    numeroMenorPositivo=0;
+    numeroMenorNegativo=0;
     i=0;
     pares=0;
+    numeroEntre=0;
     suma=0;
 
     do{
@@ -22,40 +26,97 @@ int main()
 
         while(numero==0)
         {
+
             printf("Reingrese numero: ");
             scanf("%d", &numero);
-        }
 
-    i++;
+        }
 
     if(numero%2==0)
     {
+
         pares++;
+
     }
 
     if(numero>0)
     {
+
         positivos=positivos+numero;
+
     }
 
     if(numero<0)
     {
+
         negativos=negativos+numero;
-    }
 
-    if(numero<min)
+    }
+    if(flag==0)
     {
+        max=numero;
         min=numero;
+        flag++;
+    }else
+    {
+        if(numero<min)
+        {
+            min=numero;
+
+            if(numero>0)
+            {
+                numeroMenorPositivo=numero;
+            }else
+            {
+                if(numero<0)
+                {
+                    numeroMenorNegativo=numero;
+                }
+            }
+
+        }
+
+        if(numero>max)
+        {
+            max=numero;
+
+        }
 
     }
+
+
+    if(numero>=-10&&numero<=50)
+    {
+
+        numeroEntre++;
+
+    }
+
+    suma=suma+numero;
+    i++;
 
     fflush(stdin);
+
     printf("desea continuar?");
     scanf("%c", &respuesta);
+    system("cls");
+    }while(respuesta!='n');
 
-    }while(respuesta!='s');
+    promedio=(float)suma/i;
+    promedioNegativos=(float)negativos/i;
 
+    system("cls");
 
+    printf("La cantidad de numero ingresado es: %d\n" ,i);
+    printf("La cantidad de pares: %d\n" ,pares);
+    printf("La suma de los positivos: %d\n" ,positivos);
+    printf("Promedio de los negativos: %f\n" ,promedioNegativos);
+    printf("Maximos: %d\n",max);
+    printf("Minimos: %d\n",min);
+    printf("El positivos mas chico: %d\n" ,numeroMenorPositivo);
+    printf("Negativos mas grande: %d\n" ,numeroMenorNegativo);
+    printf("Cantidad de numero entre -10 y 50: %d\n" ,numeroEntre);
+    printf("promedio total: %f\n" ,promedio);
 
 
 
