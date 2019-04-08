f69dbfab2eef55e2d8d93df90bc6c0285e37351f
alumno <alumno>
2019-04-08 12:10:51 -0300

    08/07

--- Nueva carpeta/Clase 6/main.c
+++ Nueva carpeta/Clase 6/main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define T 10
 
 
 int main()
 {
-    char palabra[T];
+    int len,  comparacion;
+    char palabra[T], palabraDos[T];
 
     printf("Ingrese un nombre:");
 
-    fgets(palabra ,T ,stdin);
+    //fgets(palabra ,T ,stdin);
     //scanf("%[^\n]", palabra);
-    //gets(palabra);
-
-    printf("%s", palabra);
-
+    gets(palabra);
+    len=strlen(palabra);
+    //strcpy(palabraDos,palabra);
+    //strncpy(palabraDos,palabra,2);
+    //comparacion=strcmp(palabra,"philipa");
+    comparacion=stricmp(palabra,"philipa");
+    printf("%s\n", palabra);
+    //printf("%s\n", palabraDos);
+    printf("%d\n", len);
+    printf("%d\n", comparacion);
 
     return 0;
 }
