57f0854e1f100ff7d6ab9233b0edeee6ec2e1ba8
alumno <alumno>
2019-04-29 11:39:13 -0300

    Signed-off-by: alumno <alumno>

--- Nueva carpeta/Clase 8/Empleado.h
+++ Nueva carpeta/Clase 8/Empleado.h
@@ -29,19 +29,24 @@ typedef struct
 
 void inicializarEmpleados(eEmpleado[], int);
 char opciones(char[]);
+
 int buscarLibre(eEmpleado[], int);
 void crearEmpleado(eEmpleado[], eSector[], int);
+
 void mostrarEmpleados(eEmpleado[], eSector[], int);
 void mostrarEmpleado(eEmpleado[], eSector[], int);
 void desplegarDatos(eEmpleado, eSector[]);
+
 void buscarModificarSueldo(eEmpleado[], eSector[], int);
 void bajaEmpleado(eEmpleado[], int);
 int pedirEntero(char texto[]);
 int buscarUno(eEmpleado[], int, int);
 float modificarSueldo(eEmpleado, eSector[]);
+
 void informeEmpleado(eEmpleado[], eSector[], int);
 eEmpleado buscarMaximo(eEmpleado[], int);
 int buscarCarlos(eEmpleado[], int);
+
 void mostrarSectores(eEmpleado[], eSector[], int);
 void mostrarSectorEmpleado(eEmpleado[], eSector[], int, int);
 void informeSectores(eEmpleado[], eSector[], int);
